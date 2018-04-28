a888f89c5be29e9b0b83bb7a116994726dd3e1db
Tricky1975 <facebook@tbbs.nl>
2018-04-28 19:34:07 +0200

    Ping!

--- netwerk.go
+++ netwerk.go
@@ -19,11 +19,6 @@ func netget(url string) string{
 	return string(body)
 }
 
-// Pings to the GJ server
-// It is adviced to do this every 30 seconds
-// Please note that it might be possible this is brought under a "go" sequence in future versions, and by that time this will be an empty function to prevent errors. This is however not a promise ;)
-func Ping(){
-	}
 
 
 func gjrequest(action,querystring string) map[string] string{
@@ -49,6 +44,13 @@ func gjrequest(action,querystring string) map[string] string{
 func (self *GJUser) qreq(action,querystring string) map[string] string{
 	return gjrequest(action,querystring+self.idstring+self.gamestuff)
 }
+
+// Pings to the GJ server
+// It is adviced to do this every 30 seconds
+// Please note that it might be possible this is brought under a "go" sequence in future versions, and by that time this will be an empty function to prevent errors. This is however not a promise ;)
+func (me *GJUser) Ping(){
+	me.qreq("sessions/ping","")
+	}
 
 
 // https://gist.github.com/sergiotapia/8263278
