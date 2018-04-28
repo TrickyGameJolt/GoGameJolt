e81d874bc74d8d366001238ff3398100a2e0699e
Tricky1975 <facebook@tbbs.nl>
2018-04-28 19:49:24 +0200

    Ping movement -- removal from old

--- netwerk.go
+++ netwerk.go
@@ -45,12 +45,6 @@ func (self *GJUser) qreq(action,querystring string) map[string] string{
 	return gjrequest(action,querystring+self.idstring+self.gamestuff)
 }
 
-// Pings to the GJ server
-// It is adviced to do this every 30 seconds
-// Please note that it might be possible this is brought under a "go" sequence in future versions, and by that time this will be an empty function to prevent errors. This is however not a promise ;)
-func (me *GJUser) Ping(){
-	me.qreq("sessions/ping","")
-	}
 
 
 // https://gist.github.com/sergiotapia/8263278
