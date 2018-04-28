91c508f1404cb221729d04b069cad3f3842a5bb2
Tricky1975 <facebook@tbbs.nl>
2018-04-28 17:52:10 +0200

    Skeleton code for user/trophy

---
+++ gebruikers.go
@@ -0,0 +1,14 @@
+package gj
+
+
+// This type is used to log in a user, and also contains the methods to send all the data a user can send or receive from Game Jolt through this API.
+type GJUser struct{
+	userid string
+	token string
+	gameid string
+	gamesig string
+}
+
+
+
+
---
+++ troffee.go
@@ -0,0 +1,7 @@
+package gj
+
+// Awards a trophy and returns "true" if succesful!
+func (*GJUser) AwardTrophy(id string) bool{
+	return false // temp line
+}
+
