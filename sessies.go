9ad96f497e4be04b37d9c11cd963bef638152523
Tricky1975 <facebook@tbbs.nl>
2018-04-28 19:45:40 +0200

    Session done

--- sessies.go
+++ sessies.go
@@ -7,3 +7,13 @@ func (me *GJUser) Ping(){
 	me.qreq("sessions/ping","")
 	}
 
+// Opens a game session for a particular user. Allows you to tell Game Jolt that a user is playing your game. You must ping the session to keep it active and you must close it when you're done with it. Note that you can only have one open session at a time. If you try to open a new session while one is running, the system will close out your current one before opening a new one.
+func (me *GJUser) OpenSession(){
+	me.qreq("sessions/open","")
+}
+
+// Closes the active session.
+
+func (me *GJUser) OpenSession(){
+	me.qreq("sessions/close","")
+}
